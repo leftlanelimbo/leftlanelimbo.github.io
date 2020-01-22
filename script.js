c88b3a201a633acfa386749fc3491285a55c55c3
Alexander Jose Flores <ajflores1604@gmail.com>
2020-01-21 18:33:05 -0600

    rotate

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ function main() {
       // uv.y = ssX;
       // Time varying pixel color
       // vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
-      // vec3 col = 0.5 + 0.5*cos(vec3(ssB*-.02,ssA*.02,0.0)+uv.xyx+vec3(0,2,4)); //no time
-      vec3 col = 0.5 + 0.5*cos(iTime+abs(ssB*0.02+ssA*0.02)+uv.xyx+vec3(1,5,9));
+      vec3 col = 0.5 + 0.5*cos(vec3(ssB*-.02,ssA*.02,0.0)+uv.xyx+vec3(0,2,4)); //no time
+      
       
 
       // Output to screen
