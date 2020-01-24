94c182809a5f6f136709aa59f8b516eac6415410
Alexander Jose Flores <ajflores1604@gmail.com>
2020-01-24 16:55:31 -0600

    shader derivative

---
+++ script.js
@@ -0,0 +1,174 @@
+function main() {
+  //:::::: three scene 
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({canvas});
+  renderer.autoClearColor = false;
+
+  const camera = new THREE.OrthographicCamera(
+    -1, // left
+     1, // right
+     1, // top
+    -1, // bottom
+    -1, // near,
+     1, // far
+  );
+  const scene = new THREE.Scene();
+  const plane = new THREE.PlaneBufferGeometry(2, 2);
+
+  const fragmentShader = `
+  #include <common>
+
+  uniform vec3 iResolution;
+  uniform float iTime;
+  uniform float ssX;
+  uniform float ssY;
+  uniform float ssA;
+  uniform float ssB;
+
+  float ssA_last = 0.0;
+  float dssA = 0.0;
+
+  float dssA_last = 0.0;
+  float ddssA = 0.0;
+
+
+  // By iq: https://www.shadertoy.com/user/iq  
+  // license: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+  void mainImage( out vec4 fragColor, in vec2 fragCoord )
+  {
+      // Normalized pixel coordinates (from 0 to 1)
+      vec2 uv = fragCoord/iResolution.xy;
+
+      //self rolled derivative function
+      dssA = (ssA-ssA_last);
+      ssA_last = ssA;
+
+      //self rolled double derivative funciton
+      ddssA = (dssA_last-dssA);
+      dssA_last = dssA;
+
+      //deviceMotion tweaks
+      // uv.y = ssX;
+      // Time varying pixel color
+      // vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+      // vec3 col = 0.5 + 0.5*cos(vec3(ssB*-.03,ssA*.03,0.0)+uv.xyx+vec3(0,2,4)); //no time
+      // vec3 col = 0.8 + 0.5*cos(vec3(ssB*-.03,ssA*.03,0.0)+uv.xyx+vec3(0,2,4)); //no time+pastel
+
+      // vec3 col = 0.03*ssA + 0.5*cos(vec3(ssB*-.03,ssA*.03,0.0)+uv.xyx+vec3(0,2,4)); //no time+pastel+direct
+      vec3 col = dssA + 0.5*cos(vec3(ssB*-.03,ssA*.03,0.0)+uv.xyx+vec3(0,2,4)); //no time+pastel+derivative?
+      // vec3 col = ddssA + 0.5*cos(vec3(ssB*-.03,ssA*.03,0.0)+uv.xyx+vec3(0,2,4)); //no time+pastel+doublederivative?
+      
+      
+
+      // Output to screen
+      fragColor = vec4(col,1.0);
+  }
+
+  void main() {
+    mainImage(gl_FragColor, gl_FragCoord.xy);
+  }
+  `;
+  const uniforms = {
+    iTime: { value: 0 },
+    iResolution:  { value: new THREE.Vector3() },
+    ssX: { type: "f", value: 0.0 },
+    ssY: { type: "f", value: 0.0 },
+    ssA: { type: "f", value: 0.0 },
+    ssB: { type: "f", value: 0.0 }
+  };
+  const material = new THREE.ShaderMaterial({
+    fragmentShader,
+    uniforms,
+  });
+  scene.add(new THREE.Mesh(plane, material));
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function render(time) {
+    time *= 0.001;  // convert to seconds
+
+    resizeRendererToDisplaySize(renderer);
+
+    const canvas = renderer.domElement;
+    uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
+    uniforms.iTime.value = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+  //:::::: end three scene 
+
+  //:::::: device motion
+  
+  //initialize smoothing variables
+  let sX = 0.0;
+  let sY = 0.0;
+  let sB = 0.0; // need to intialize to value you want it to start close to 
+  let sA = 0.0;
+  let smoothing_factor = 0.9;
+
+  function handleMotionEvent(event) {
+    // t = event.acceleration;
+    // console.log(t);
+    x = event.accelerationIncludingGravity.x; //left-right tilt (0@ table plane)
+    y = event.accelerationIncludingGravity.y; //front-back tilt (0@ table plane)
+    // z = event.accelerationIncludingGravity.z; //compliement to y (0 @ wall plane)
+
+    a = event.rotationRate.alpha; //front-back accel (on table plane)
+    b = event.rotationRate.beta; //left-right accel (on table plane)
+    // g = event.rotationRate.gamma; //left-right accel (on wall plane)
+
+
+    sX = x + smoothing_factor * (sX - x);
+    sY = y + smoothing_factor * (sY - y);
+
+    sB = b + smoothing_factor * (sB - b);
+    sA = a + smoothing_factor * (sA - a);
+    // console.log(sX,sY,sB,sA);
+
+    //passing to material uniforms
+    material.uniforms.ssX.value = sX;
+    material.uniforms.ssY.value = sY;
+    material.uniforms.ssA.value = sA;
+    material.uniforms.ssB.value = sB;
+  }
+
+
+  //request device motion and call handlers
+  //https://developer.mozilla.org/en-US/docs/Web/API/DeviceMotionEvent
+  function onClickDeviceMotion() {
+    // feature detect
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+      DeviceMotionEvent.requestPermission()
+        .then(permissionState => {
+          if (permissionState === 'granted') {
+            window.addEventListener('devicemotion', handleMotionEvent, true);
+
+          }
+        })
+        .catch(console.error);
+    } else {
+      // handle regular non iOS 13+ devices
+      window.addEventListener('devicemotion', handleMotionEvent, true);
+      console.log(console.error);
+    }
+  }
+
+
+  document.getElementById('motion').addEventListener("click", onClickDeviceMotion);
+
+}
+
+main();
