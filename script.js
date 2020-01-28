f22390c034f32c259a00abc3b575eb69b86ac1bc
Alexander Jose Flores <ajflores1604@gmail.com>
2020-01-27 22:53:22 -0600

    spacial_audio_parallel_audio

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function main() {
 
     async function loadAndPlay(){
       //parallel promise define
-      let promises = [loadSound('atArmsLengthLM.mp3', light), loadSound('atArmsLengthRM.mp3', light2)];
+      let promises = [loadSound('atArmsLengthLJS.mp3', light), loadSound('atArmsLengthRJS.mp3', light2)];
       //parallel process load
       Promise.all(promises).then((results)=>{
         for(let i = 0;i<results.length;i++){
