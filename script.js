c3a46115fe53116e0427ae4a9b2a5674be074f47
Alexander Jose Flores <ajflores1604@gmail.com>
2020-02-11 17:29:22 -0600

    3d stems

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function main() {
   const renderer = new THREE.WebGLRenderer({ canvas, antialias: true});
   renderer.autoClearColor = false;
 
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(105, window.innerWidth / window.innerHeight, 0.1, 1000);
   const scene = new THREE.Scene();
 
   //black plane
